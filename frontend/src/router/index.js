19c53cba4787bd651422a2dba4592ef1583f7d95
Hoda Akrami <Hakrami3257@conestogac.on.ca>
2023-01-31 14:56:03 -0500

    fixed merge conflict

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,82 @@ import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+// **This is the default code for this file
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+//   ]
+// })
+// **End of the code
+
+const routes = [
+  {
+    // Should remove later
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  }
+  // ** Comment them out whenever we actually made the pages
+  // {
+  //   path: '/login',
+  //   name: 'Login',
+  //   component: Login
+
+  // },
+  // {
+  //   path: '/dashboard',
+  //   name: 'Dashboard',
+  //   component: Dashboard
+
+  // },
+  // {
+  //   path: '/jobRecords',
+  //   name: 'JobRecords',
+  //   component: JobRecords
+
+  // },
+  // {
+  //   path: '/calender',
+  //   name: 'Calender',
+  //   component: Calender
+
+  // },
+  // {
+  //   path: '/resources',
+  //   name: 'Resources',
+  //   component: Resources
+  // },
+  // {
+  //   path: '/portfolios',
+  //   name: 'Portfolios',
+  //   component: Portfolios
+
+  // },
+  // {
+  //   path: '/resumeBuilder',
+  //   name: 'ResumeBuilder',
+  //   component: ResumeBuilder
+
+  // },
+  // {
+  //   path: '/communityBlog',
+  //   name: 'CommunityBlog',
+  //   component: CommunityBlog
+
+  // }
+]
+
+// Create a history that a user can go back to and construct a router object for Vue, respectively.
+// To create our router
+const router = new Router({
+  mode: 'history',
+  routes
 })
+
+// To export our router
+// We can access this component from another file by simply referencing the file with any name since a function is exported by default.
+export default router

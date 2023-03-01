4c28128987cc985290dc5c5efe00f66a8a710683
Hoda Akrami <Hakrami3257@conestogac.on.ca>
2023-03-01 09:53:01 -0500

    added a basic full calendar

--- frontend/src/views/CalendarView.vue
+++ frontend/src/views/CalendarView.vue
@@ -1,9 +1,36 @@
 <template>
-<div>
-  <h1 class="view-title">Calendar</h1>
-</div>
+  <FullCalendar :options="calendarOptions" />
 </template>
-
 <script>
-</script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
 
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        events: [
+          { title: 'event 1', date: '2019-04-01' },
+          { title: 'event 2', date: '2019-04-02' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleDateClick: function(arg) {
+      const title = prompt('Enter event title:')
+      if (title) {
+        const newEvent = { title, date: arg.dateStr }
+        this.calendarOptions.events.push(newEvent)
+      }
+    }
+  }
+}
+</script>

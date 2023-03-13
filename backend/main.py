a3ca4d85a2880e3bf66741bfc848f7f22294ca5f
Hoda Akrami <Hakrami3257@conestogac.on.ca>
2023-03-13 11:24:24 -0400

    fixed the merge conflict

--- backend/main.py
+++ backend/main.py
@@ -5,20 +5,12 @@ from sqlalchemy import event
 from core.config import settings
 from db.db_setup import engine, Base
 from db.models import user_model, job_record_model, job_tag_model, job_status_model, \
-<<<<<<< HEAD
-    resource_type_model, resource_model, resource_extension_type_model, portfolio_model, event_model
-from api import user_router, auth_router, job_record_router, job_tag_router, job_status_router, \
-    resource_type_router, resource_router, resource_extension_type_router, portfolio_router, event_router
-from api.services import resource_type_service, resource_extension_type_service, \
-    job_status_service, job_tag_service, event_service
-=======
     resource_type_model, resource_model, resource_extension_type_model, portfolio_model, job_note_model, \
-    job_note_type_model
+    job_note_type_model, event_model
 from api import user_router, auth_router, job_record_router, job_tag_router, job_status_router, \
-    resource_type_router, resource_router, resource_extension_type_router, portfolio_router, dashboard_router
+    resource_type_router, resource_router, resource_extension_type_router, portfolio_router, dashboard_router, event_router
 from api.services import resource_type_service, resource_extension_type_service, \
-    job_status_service, job_tag_service, job_note_type_service
->>>>>>> main
+    job_status_service, job_tag_service, job_note_type_service, event_service   
 
 
 # Event listener that is executed after create_all method has been called for tables

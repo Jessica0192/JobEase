d5bbf5cc500faa9d35bff56b27d10ea25467ee5e
Enes Demirsoz <64825806+EDemirsoz@users.noreply.github.com>
2023-03-26 23:47:33 -0400

    handle user auth with email and revoke

--- backend/api/google_auth_and_calendar_router.py
+++ backend/api/google_auth_and_calendar_router.py
@@ -2,8 +2,12 @@ import os
 import fastapi
 import requests
 
+from sqlalchemy.orm import Session
+from db.db_setup import get_db
+from fastapi import Depends, HTTPException, status
 from fastapi.responses import RedirectResponse
-from api.services import google_service
+from db.models.user_model import User
+from api.services import google_service, user_service, auth_service
 from google_auth_oauthlib.flow import Flow
 
 router = fastapi.APIRouter(
@@ -16,7 +20,9 @@ CLIENT_SECRETS_FILE = os.path.join(os.path.dirname(__file__), '..', 'client_secr
 # Authorized redirect URI from Google Cloud Console
 REDIRECT_URI = "http://localhost:8000/google/oauth2callback"
 # The scopes for Google Calendar API
-SCOPES = ["https://www.googleapis.com/auth/calendar.events",
+SCOPES = ["openid",
+          "https://www.googleapis.com/auth/userinfo.email",
+          "https://www.googleapis.com/auth/calendar.events",
           "https://www.googleapis.com/auth/calendar",
           "https://www.googleapis.com/auth/userinfo.profile"]
 
@@ -36,39 +42,56 @@ async def google_authenticate():
 
 
 @router.get("/oauth2callback")
-async def callback(code: str):
+async def callback(code: str,
+                   db: Session = Depends(get_db)):
     flow = Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES, redirect_uri=REDIRECT_URI)
     flow.fetch_token(code=code)
 
     credentials = flow.credentials
 
     # Get user info using access token
-    url = "https://www.googleapis.com/oauth2/v3/userinfo"
-    headers = {
-        "Authorization": f"Bearer {credentials.token}"
-    }
-    response = requests.get(url, headers=headers)
+    url = "https://www.googleapis.com/oauth2/v3/userinfo?access_token=" + credentials.token
+
+    response = requests.get(url)
 
     if response.status_code == 200:
         user_info = response.json()
-        userid = user_info["given_name"] + user_info["sub"]
-        google_service.store_google_credentials(credentials, userid)
+        db_user = user_service.get_user_by_email(db=db, email=user_info["email"])
+
+        if db_user is None:
+            await revoke_credentials_helper(credentials)
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User with provided gmail not found")
+
+        google_service.store_google_credentials(credentials, db_user.email)
         return "Google Authentication is successful"
     else:
         print(f"Error: {response.status_code} - {response.text}")
         return "Google Authentication has failed!"
 
 
-@router.get("/revoke")
-async def revoke_credentials():
-    credentials = google_service.get_google_credentials("enes")
+@router.delete("revoke")
+async def revoke_credentials(current_user: User = Depends(auth_service.get_current_user_from_token)):
+    credentials = google_service.get_google_credentials(current_user.email)
+
+    if credentials is False:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Google Credentials not found")
+
+    result = await revoke_credentials_helper(credentials)
+
+    if result is True:
+        google_service.remove_google_credentials(current_user.email)
+        return 'Google Credentials successfully revoked.'
+    else:
+        return 'An error occurred while revoking Google credentials.'
+
+
+async def revoke_credentials_helper(credentials):
     revoke = requests.post('https://oauth2.googleapis.com/revoke',
                            params={'token': credentials.token},
                            headers={'content-type': 'application/x-www-form-urlencoded'})
 
     status_code = revoke.status_code
     if status_code == 200:
-        google_service.remove_google_credentials("enes")
-        return 'Google Credentials successfully revoked.'
+        return True
     else:
-        return 'An error occurred while revoking Google credentials.'
+        return False
--- backend/api/services/user_service.py
+++ backend/api/services/user_service.py
@@ -17,6 +17,10 @@ def get_user_by_username(db: Session, username: str):
     return db.query(User).filter(User.username == username).first()
 
 
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
+
+
 def delete_user_by_id(db: Session, user_id: int):
     existing_user = db.query(User).filter(User.id == user_id)
     if not existing_user.first():
